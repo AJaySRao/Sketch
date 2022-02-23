533d7f667f1170ea14df560f3971cac4d73c36a1
changeAJay <ajajay008@gmail.com>
2022-02-23 05:53:23 +0530

    Used higher order functions and Event listeners to move turtle object

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+t = Turtle()
+s = Screen()
+s.listen()
+
+
+def move_forward():
+    t.forward(10)
+
+
+def move_backward():
+    t.back(10)
+
+def turn_left():
+    t.left(10)
+
+def turn_right():
+    t.right(10)
+
+def all_clear():
+    t.clear()
+    t.reset()
+
+s.onkey(key='w', fun=move_forward)
+s.onkey(key='s', fun=move_backward)
+s.onkey(key='a', fun=turn_left)
+s.onkey(key='d', fun=turn_right)
+s.onkey(key='c', fun=all_clear)
+
+s.exitonclick()
